b76a35bfa024a73e570dd51992a810684678bde7
Bruno Sales <me@baliestri.dev>
2026-01-22 20:12:22 -0300

    feat: extract IStabilizedReader interface

---
+++ AssuanLibrary/Network/Utility/IStabilizedReader.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+namespace AssuanLibrary.Network.Utility;
+
+/// <summary>
+///   A reader that reads data from a stream until it stabilizes.
+/// </summary>
+public interface IStabilizedReader : IDisposable {
+  /// <summary>
+  ///   The duration to wait after the last received data chunk before considering the stream to be stable. Defaults to 150 milliseconds.
+  /// </summary>
+  TimeSpan StabilizationDelay { get; set; }
+
+  /// <summary>
+  ///   The grace period to wait after reading data before checking for more data. Defaults to 40 milliseconds.
+  /// </summary>
+  TimeSpan ReadGracePeriod { get; set; }
+
+  /// <summary>
+  ///   The interval between polls to check for available data. Defaults to 50 milliseconds.
+  /// </summary>
+  TimeSpan PollInterval { get; set; }
+
+  /// <summary>
+  ///   Reads data from the stream until it stabilizes or the timeout is reached.
+  /// </summary>
+  /// <returns>The read data as a byte array.</returns>
+  /// <exception cref="TimeoutException">Thrown if the read operation times out.</exception>
+  byte[] Read();
+
+  /// <summary>
+  ///   Asynchronously reads data from the stream until it stabilizes or the timeout is reached.
+  /// </summary>
+  /// <param name="ct">A <see cref="CancellationToken" /> to observe while waiting for the task to complete.</param>
+  /// <returns>A value task that represents the asynchronous read operation. The value of the TResult parameter contains the read data as a byte array.</returns>
+  /// <exception cref="TimeoutException">Thrown if the read operation times out.</exception>
+  ValueTask<byte[]> ReadAsync(CancellationToken ct = default);
+}

cfd70bedb9f3d3adae1252fe6106d1cd7867b93f
Bruno Sales <me@baliestri.dev>
2026-01-22 19:13:58 -0300

    refactor: update AssuanClientOptions to use SocketDescriptor instead of IPAddress and PortAndNonce

--- AssuanLibrary/Network/AssuanClientOptions.cs
+++ AssuanLibrary/Network/AssuanClientOptions.cs
@@ -1,75 +1,36 @@
 // Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
-using System.Net;
-
 namespace AssuanLibrary.Network;
 
 /// <summary>
 ///   Represents configuration options for an Assuan client.
 /// </summary>
-/// <param name="ipAddress">The IP address to connect to.</param>
-/// <param name="portAndNonce">The port and nonce information to use when connecting.</param>
+/// <param name="socketDescriptor">The socket descriptor to use when connecting.</param>
 /// <param name="timeout">The timeout duration for receiving data.</param>
-public sealed class AssuanClientOptions(IPAddress ipAddress, PortAndNonce portAndNonce, TimeSpan timeout) : IEquatable<AssuanClientOptions> {
+public sealed class AssuanClientOptions(SocketDescriptor socketDescriptor, TimeSpan timeout) : IEquatable<AssuanClientOptions> {
   /// <summary>
   ///   A read-only instance of <see cref="AssuanClientOptions" /> with default values.
   /// </summary>
   public static readonly AssuanClientOptions Empty = new();
 
   /// <summary>
-  ///   Initializes a new instance of the <see cref="AssuanClientOptions" /> class with the specified port and nonce
-  ///   and timeout, using the loopback IP address.
-  /// </summary>
-  /// <param name="portAndNonce">The port and nonce information to use when connecting.</param>
-  /// <param name="timeout">The timeout duration for receiving data.</param>
-  public AssuanClientOptions(PortAndNonce portAndNonce, TimeSpan timeout) : this(IPAddress.Loopback, portAndNonce, timeout) { }
-
-  /// <summary>
   ///   Initializes a new instance of the <see cref="AssuanClientOptions" /> class with the specified port and nonce,
   ///   using the loopback IP address.
   /// </summary>
-  /// <param name="portAndNonce">The port and nonce information to use when connecting.</param>
-  public AssuanClientOptions(PortAndNonce portAndNonce) : this(IPAddress.Loopback, portAndNonce, TimeSpan.FromSeconds(DEFAULT_TIMEOUT_SECONDS)) { }
+  /// <param name="socketDescriptor">The socket descriptor to use when connecting.</param>
+  public AssuanClientOptions(SocketDescriptor socketDescriptor) : this(socketDescriptor, TimeSpan.FromSeconds(DEFAULT_TIMEOUT_SECONDS)) { }
 
   /// <summary>
   ///   Initializes a new instance of the <see cref="AssuanClientOptions" /> class using the loopback IP address
   ///   and an empty port and nonce.
   /// </summary>
-  public AssuanClientOptions() : this(IPAddress.Loopback, PortAndNonce.Empty, TimeSpan.FromSeconds(DEFAULT_TIMEOUT_SECONDS)) { }
-
-  /// <summary>
-  ///   The IP address to connect to.
-  /// </summary>
-  public IPAddress IpAddress {
-    get;
-    set {
-      if (Equals(value, IPAddress.None) ||
-          Equals(value, IPAddress.IPv6None)) {
-        throw new NotSupportedException("The specified IP address is not supported.");
-      }
-
-      field = value;
-    }
-  } = ipAddress;
+  public AssuanClientOptions() : this(SocketDescriptor.AgentSocket, TimeSpan.FromSeconds(DEFAULT_TIMEOUT_SECONDS)) { }
 
   /// <summary>
   ///   The port and nonce information to use when connecting.
   /// </summary>
-  public PortAndNonce PortAndNonce {
-    get;
-    set {
-      if (value.Port <= 0) {
-        throw new ArgumentOutOfRangeException(nameof(PortAndNonce.Port), "The port must be a positive ushort.");
-      }
-
-      if (value.Nonce.Length == 0) {
-        throw new ArgumentException("The nonce cannot be empty.", nameof(PortAndNonce.Nonce));
-      }
-
-      field = value;
-    }
-  } = portAndNonce;
+  public SocketDescriptor SocketDescriptor { get; set; } = socketDescriptor;
 
   /// <summary>
   ///   The timeout duration for receiving data.
@@ -91,20 +52,17 @@ public sealed class AssuanClientOptions(IPAddress ipAddress, PortAndNonce portAn
       return true;
     }
 
-    return IpAddress.Equals(other.IpAddress) &&
-           PortAndNonce.Equals(other.PortAndNonce) &&
+    return SocketDescriptor.Equals(other.SocketDescriptor) &&
            ThrowIfNotConnected == other.ThrowIfNotConnected;
   }
 
   /// <summary>
   ///   Deconstructs the <see cref="AssuanClientOptions" /> into its components.
   /// </summary>
-  /// <param name="ipAddress">The IP address.</param>
-  /// <param name="portAndNonce">The port and nonce information.</param>
+  /// <param name="socketDescriptor">The socket descriptor.</param>
   /// <param name="timeout">The timeout duration.</param>
-  public void Deconstruct(out IPAddress ipAddress, out PortAndNonce portAndNonce, out TimeSpan timeout) {
-    ipAddress = IpAddress;
-    portAndNonce = PortAndNonce;
+  public void Deconstruct(out SocketDescriptor socketDescriptor, out TimeSpan timeout) {
+    socketDescriptor = SocketDescriptor;
     timeout = Timeout;
   }
 
@@ -137,8 +95,7 @@ public sealed class AssuanClientOptions(IPAddress ipAddress, PortAndNonce portAn
     => !Equals(left, right);
 
   private IEnumerable<object> GetEqualityComponents() {
-    yield return IpAddress;
-    yield return PortAndNonce;
+    yield return SocketDescriptor;
     yield return Timeout;
     yield return ThrowIfNotConnected;
   }

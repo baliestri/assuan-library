0f1e29203cdb43af958cf0fb8d5e8bc2d2fe9ff7
Bruno Sales <me@baliestri.dev>
2026-01-22 19:08:20 -0300

    refactor: implement IEquatable for SocketDescriptor to enhance equality comparison

--- AssuanLibrary/Network/SocketDescriptor.cs
+++ AssuanLibrary/Network/SocketDescriptor.cs
@@ -6,7 +6,7 @@ namespace AssuanLibrary.Network;
 /// <summary>
 ///   Represents a smart enum for which socket file to use.
 /// </summary>
-public sealed class SocketDescriptor {
+public sealed class SocketDescriptor : IEquatable<SocketDescriptor> {
   /// <summary>
   ///   Uses <c>agent-socket</c> as the socket descriptor.
   /// </summary>
@@ -28,8 +28,47 @@ public sealed class SocketDescriptor {
     => _descriptor = descriptor;
 
   /// <inheritdoc />
+  public bool Equals(SocketDescriptor? other) {
+    if (other is null) {
+      return false;
+    }
+
+    if (ReferenceEquals(this, other)) {
+      return true;
+    }
+
+    return _descriptor == other._descriptor;
+  }
+
+  /// <inheritdoc />
+  public override bool Equals(object? obj)
+    => ReferenceEquals(this, obj) || (obj is SocketDescriptor other && Equals(other));
+
+  /// <inheritdoc />
+  public override int GetHashCode()
+    => _descriptor.GetHashCode();
+
+  /// <inheritdoc />
   public override string ToString()
     => _descriptor;
+
+  /// <summary>
+  ///   Determines whether two <see cref="SocketDescriptor" /> instances are equal.
+  /// </summary>
+  /// <param name="left">The left instance.</param>
+  /// <param name="right">The right instance.</param>
+  /// <returns><see langword="true" /> if the two instances are equal; otherwise, <see langword="false" />.</returns>
+  public static bool operator ==(SocketDescriptor? left, SocketDescriptor? right)
+    => Equals(left, right);
+
+  /// <summary>
+  ///   Determines whether two <see cref="SocketDescriptor" /> instances are not equal.
+  /// </summary>
+  /// <param name="left">The left instance.</param>
+  /// <param name="right">The right instance.</param>
+  /// <returns><see langword="true" /> if the two instances are not equal; otherwise, <see langword="false" />.</returns>
+  public static bool operator !=(SocketDescriptor? left, SocketDescriptor? right)
+    => !Equals(left, right);
 
   /// <summary>
   ///   Implicitly converts a <see cref="SocketDescriptor" /> to its underlying string representation.

9e562be97edcadf3637483f3cfcbe93a62e570aa
Bruno Sales <me@baliestri.dev>
2026-01-23 15:08:13 -0300

    feat: add additional byte constants for character representation in AssuanPredefined

--- AssuanLibrary/AssuanPredefined.cs
+++ AssuanLibrary/AssuanPredefined.cs
@@ -10,5 +10,15 @@ internal static class AssuanPredefined {
   public static class Characters {
     public const byte SPACE = 0x20;
     public const byte LINE_FEED = 0x0A;
+    public const byte CARRIAGE_RETURN = 0x0D;
+    public const byte TABULATION = 0x09;
+    public const byte OPEN_PARENTHESIS = 0x28;
+    public const byte CLOSE_PARENTHESIS = 0x29;
+    public const byte COLON = 0x3A;
+
+    public const byte DIGIT_ZERO = 0x30;
+    public const byte DIGIT_NINE = 0x39;
+    public const byte BINARY_LENGTH_CONTINUATION_BIT = 0x80;
+    public const byte BINARY_LENGTH_VALUE_MASK = 0x7F;
   }
 }

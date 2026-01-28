379b4ab2621251b2d3d6c671b526d1f415b4fe5e
Bruno Sales <me@baliestri.dev>
2026-01-28 20:03:08 -0300

    refactor: update StabilizationIdleDetector to improve idle state management

--- AssuanLibrary/Transport/IO/StabilizationIdleDetector.cs
+++ AssuanLibrary/Transport/IO/StabilizationIdleDetector.cs
@@ -7,26 +7,30 @@ namespace AssuanLibrary.Transport.IO;
 
 [ExcludeFromCodeCoverage]
 internal static class StabilizationIdleDetector {
-  public static void UpdateZeroState(int available, ref int consecutiveZeros, ref DateTime zeroStartedAt) {
-    if (available == 0) {
-      consecutiveZeros++;
-      if (consecutiveZeros == 1) {
-        zeroStartedAt = DateTime.UtcNow;
-      }
-
+  /// <summary>
+  ///   Updates the idle detector state based on whether data was received.
+  /// </summary>
+  /// <param name="hadData">Indicates whether data was received during the last check.</param>
+  /// <param name="elapsed">The elapsed time since the last check.</param>
+  /// <param name="idleElapsed">The elapsed idle time.</param>
+  /// <param name="idleCycles">The number of idle cycles.</param>
+  public static void Update(bool hadData, TimeSpan elapsed, ref TimeSpan idleElapsed, ref int idleCycles) {
+    if (hadData) {
+      idleElapsed = TimeSpan.Zero;
+      idleCycles = 0;
       return;
     }
 
-    consecutiveZeros = 0;
-    zeroStartedAt = default;
+    idleElapsed += elapsed;
+    idleCycles++;
   }
 
-  public static bool IsStableIdle(int consecutiveZeros, DateTime zeroStartedAt, TimeSpan stabilizationDelay) {
-    if (consecutiveZeros == 0) {
-      return false;
-    }
-
-    var elapsed = DateTime.UtcNow - zeroStartedAt;
-    return elapsed >= stabilizationDelay;
-  }
+  /// <summary>
+  ///   Determines whether the idle time has reached the required delay.
+  /// </summary>
+  /// <param name="idleElapsed">The elapsed idle time.</param>
+  /// <param name="requiredDelay">The required delay to consider the state as stable.</param>
+  /// <returns><see langword="true" /> if the idle time has reached the required delay; otherwise, <see langword="false" />.</returns>
+  public static bool IsStable(TimeSpan idleElapsed, TimeSpan requiredDelay)
+    => idleElapsed >= requiredDelay;
 }
